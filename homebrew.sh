d65080cb917563ab72343981ec107b79ab3ef358
William Leiby <william@williamleiby.com>
2019-04-23 09:48:26 -0500

    updated scripts

---
+++ homebrew.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+brew install node
+brew install yarn
+brew install nvm
+brew install tmux
+brew install youtube-dl
+brew install zsh
+brew install circle-ci
+brew install heroku
+brew install pipenv
+brew install postgresql
+brew install redis
+brew install go
+
+# Install MacOS Applications
+brew cask install xcode
+brew cask install lastpass
+brew cask install appcleaner
+brew cask install avast-security
+brew cask install cleanmymac
+brew cask install google-chrome
+brew cask install alfred
+brew cask install magnet
+brew cask install backblaze
+brew cask install xmind
+brew cask install dropbox
+brew cask install cocoapods
+brew cask install sequel-pro
+brew cask install transmit
+brew cask install malwarebytes
+brew cask install tunnelbear
+brew cask install kindle
+brew cask install slack
+brew cask install pdf-expert
+brew cask install itsycal
+brew cask install the-unarchiver
+brew cask install utorrent
+brew cask install speedtest
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install iterm2
+brew cask install google-backup-and-sync
+brew cask install vlc
+brew cask install timing-app
+brew cask install little-snitch
+brew cask install cocoapods
+
+# Install Fonts
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+
+# Install CocoaPods
+gem install cocoapods
