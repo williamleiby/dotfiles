be518b049cbc27a11f98607bfd34bbed60e0bcff
william leiby <william.r.leiby@gmail.com>
2019-07-24 18:18:23 -0500

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,4 @@
+git config --global user.name "William Leiby"
+git config --global user.email "william@williamleiby.com"
+git config --global color.ui auto
+git config --global core.editor 'code --wait'
