450517d8ce8d4ce7144c64f4dba70b5a7083f533
william leiby <william.r.leiby@gmail.com>
2019-07-25 11:56:37 -0500

    Update mac.sh

--- macos/mac.sh
+++ macos/mac.sh
@@ -2,6 +2,8 @@
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # Software Updates globally for macOS.
 softwareupdate -i -a
+# Show hidden files in Finder.
+defaults write com.apple.finder AppleShowAllFiles -bool TRUE
 # Key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 # Set a really fast key repeat.
@@ -12,3 +14,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 # Change screenshot directory to ~/Downloads
 defaults write com.apple.screencapture locations ~/Downloads
+# Prevent apps from saving to iCloud
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool FALSE
+# Disables Dashboard completely
+defaults write com.apple.dashboard mcx-disabled -boolean TRUE
